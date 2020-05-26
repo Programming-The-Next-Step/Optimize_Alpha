929bb3358153e88d0a1acd2cdd260a1e472013ad
Maier <maxi-maier@web.de>
2020-05-26 19:14:49 +0200

    Revert "ShinyApp"
    This reverts commit 1f75e80bdc1d35b346c660f73a5eb27e30e2609a.

--- Package/idealalphaPackage/tests/testthat/test-OptimizeR.R
+++ Package/idealalphaPackage/tests/testthat/test-OptimizeR.R
@@ -4,24 +4,21 @@ library(OptimizeR)
 context("Core OptimizeR functionality")
 
 test_that('returned alpha is correct', {
-        n <- 40
-        effsize <- 0.2
+        n <- 100
+        effsize <- 0.3
         prior <- 0.5
-        posttrue <- 0.95
-        postfalse <- 0.05
-        test <- "t-test"
-        alternative <- "greater"
-        type <- "one.sample"
+        posttrue <- 0.8
+        postfalse <- 0.2
 
+        ac <- alphaConfirmation(n, effsize, prior, posttrue)
+        ad <- alphaDisconfirmation(n, effsize, prior, postfalse)
+        pp <- probpower(effsize, prior, postfalse, posttrue)
+        ia <- idealalpha(n, effsize, prior)
 
-        ac <- alphaConfirmation(n, effsize, prior, posttrue, test = test, alternative = alternative, type = type)
-        ad <- alphaDisconfirmation(n, effsize, prior, postfalse, test = test, alternative = alternative, type = type)
-        pp <- probPower(effsize, prior, postfalse, posttrue, test = test, alternative = alternative, type = type)
-        ia <- idealAlpha(n, effsize, prior, test = test, alternative = alternative, type = type)
-        expect_equal(round(postConfirm(ac$power,ac$alpha,  prior), digits = 2), posttrue)
-        expect_equal(round(postDisconfirm(ad$power, ad$alpha, prior), digits =2), postfalse)
-        expect_equal(round(postConfirm(pp$power,pp$alpha,  prior), digits =2), posttrue)
-        expect_equal(round(postDisconfirm(pp$power,pp$alpha,  prior), digits =2), postfalse)
+        expect_equal(round(postConfirm(ac$power,ac$alpha,  prior), digits = 1), posttrue)
+        expect_equal(round(postDisconfirm(ad$power, ad$alpha, prior), digits =1), postfalse)
+        expect_equal(round(postConfirm(pp$power,pp$alpha,  prior), digits =1), posttrue)
+        expect_equal(round(postDisconfirm(pp$power,pp$alpha,  prior), digits =1), postfalse)
         expect_equal(round(information(postConfirm(ia$power, ia$alpha, prior),postDisconfirm(ia$power, ia$alpha, prior),ia$power, ia$alpha, prior),2), round(ia$Learning, digits = 2))
 })
 
